cc97e4c138072b4e3775403496dcb3d2048042a1
AureleSecG <aurele.guitard.pro@gmail.com>
2022-09-25 22:48:09 +0200

    prep

---
+++ ecommerce/src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+
+const Home = () => {
+    return (
+        <div className='container'>
+            <Navbar />
+        </div>
+    )
+}
+
+export default Home
