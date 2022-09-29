5f4da896fd0d267c11dca6da943ab1d8dcea766f
AureleSecG <aurele.guitard.pro@gmail.com>
2022-09-30 00:11:35 +0200

    Responsive Newsletter and preparing Footer

--- ecommerce/src/Components/Newsletter.jsx
+++ ecommerce/src/Components/Newsletter.jsx
@@ -7,21 +7,39 @@ const Container = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    flex-direction: column;
 `
 const Title = styled.h1`
-    
+    font-size: 70px;
+    margin-bottom: 20px;
 `
+
 const Description = styled.div`
-    
+    font-size: 24px;
+    font-weight: 300;
+    margin-bottom: 20px;
 `
+
 const InputContainer = styled.div`
-    
+    width: 50%;
+    height: 40px;
+    background-color: white;
+    display: flex;
+    justify-content: space-between;
+    border: 1px solid lightgray;
 `
+
 const Input = styled.input`
-    
+    border: none;
+    flex: 8;
+    padding-left: 20px;
 `
+
 const Button = styled.button`
-    
+    flex: 1;
+    border: none;
+    background-color: teal;
+    color: white;
 `
 
 const Newsletter = () => {
