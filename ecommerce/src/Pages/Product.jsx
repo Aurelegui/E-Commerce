52fa1ab92ad61cc644fe58b5871882436f91d73d
AureleSecG <aurele.guitard.pro@gmail.com>
2022-10-01 23:43:58 +0200

    Setup Home for Product display

--- ecommerce/src/Pages/Product.jsx
+++ ecommerce/src/Pages/Product.jsx
@@ -1,4 +1,8 @@
 import styled from 'styled-components';
+import Announcement from '../Components/Announcement';
+import Footer from '../Components/Footer';
+import Navbar from '../Components/Navbar';
+import Newsletter from '../Components/Newsletter';
 
 const Container = styled.div`
 
@@ -7,6 +11,11 @@ const Container = styled.div`
 const Product = () => {
     return (
         <Container>
+            <Navbar />
+            <Announcement />
+            <Newsletter />
+            <Footer />
+
         </Container>
     )
 }
