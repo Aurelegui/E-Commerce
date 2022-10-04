3155f04d6161ce7e0b661eba1eab180c30d4edd3
AureleSecG <aurele.guitard.pro@gmail.com>
2022-10-04 22:50:17 +0200

    Cart finished, starting responsive

--- ecommerce/src/Pages/Cart.jsx
+++ ecommerce/src/Pages/Cart.jsx
@@ -2,15 +2,165 @@ import styled from 'styled-components';
 import Announcement from '../Components/Announcement';
 import Footer from '../Components/Footer';
 import Navbar from '../Components/Navbar';
+import Remove from '@mui/icons-material/RemoveOutlined';
+import Add from '@mui/icons-material/AddOutlined';
 
 const Container = styled.div`
     
 `
+
 const Wrapper = styled.div`
+    padding: 20px;
+`
+
+const Title = styled.h1`
+    font-weight: 300;
+    text-align: center;
+`
+
+const Top = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px;
+`
+
+const TopButton = styled.button`
+    padding: 10px;
+    font-weight: 600;
+    cursor: pointer;
+    border: ${props => props.type === "filled" && "none"};
+    background-color: ${props => props.type === "filled" ? "black" : "transparent"};
+    color: ${props => props.type === "filled" && "white"};
+`
+
+const TopTexts = styled.div`
     
 `
-const Title = styled.h1`
+const TopText = styled.span`
+    text-decoration: underline;
+    cursor: pointer;
+    margin: 0px 10px;
+`
+
+const Bottom = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+
+const Info = styled.div`
+    flex: 3;
+`
+
+const Product = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+
+const ProductDetail = styled.div`
+    flex: 2;
+    display: flex;
+`
+
+const PriceDetail = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+
+const ProductAmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 20px;
+`
+
+const ProductAmount = styled.div`
+    font-size: 24px;
+    margin: 5px;
+`
+
+const ProductPrice = styled.div`
+    font-size: 30px;
+    font-weight: 200;
+`
+
+const Hr = styled.hr`
+    background-color: #eee;
+    border: none;
+    height: 1px;
+
+`
+
+const Details = styled.div`
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+`
+
+const Image = styled.img`
+    width: 300px;
+    height: auto;
+`
+
+const ProductName = styled.span`
     
+`
+
+const ProductId = styled.span`
+    
+`
+
+const ProductColor = styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${props => props.color};
+`
+
+const ProductSize = styled.span`
+    
+`
+
+const Summary = styled.div`
+    flex: 1;
+    border: 0.5px solid lightgray;
+    border-radius: 10px;
+    padding: 20px;
+    height: 50vh;
+`
+
+const SummaryTitle = styled.h1`
+    font-weight: 200;
+`
+
+const SummaryItem = styled.div`
+    margin: 30px 0px;
+    display: flex;
+    justify-content: space-between;
+    font-weight: ${props => props.type === "total" && "500"};
+    font-size: ${props => props.type === "total" && "24px"};
+`
+
+const SummaryItemText = styled.span`
+    
+`
+
+const SummaryItemPrice = styled.span`
+    
+`
+
+const SummaryButton = styled.button`
+    width: 50%;
+    font-weight: 600;
+    padding: 10px;
+    background-color: black;
+    color: white;
+    display: flex;
+    justify-content: center;
 `
 
 const Cart = () => {
@@ -20,6 +170,77 @@ const Cart = () => {
             <Announcement />
             <Wrapper>
                 <Title>YOUR BAG</Title>
+                <Top>
+                    <TopButton>CONTINUE SHOPPING</TopButton>
+                    <TopTexts>
+                        <TopText>Shopping Bag(2)</TopText>
+                        <TopText>Your Wishlist</TopText>
+                    </TopTexts>
+                    <TopButton type="filled">CHECKOUT NOW</TopButton>
+                </Top>
+                <Bottom>
+                    <Info>
+                        <Product>
+                            <ProductDetail>
+                                <Image src="https://static.nike.com/a/images/t_default/9cffba44-ab82-4920-af87-8c3478ee3659/kd15-basketball-shoes-10P3rj.png" />
+                                <Details>
+                                    <ProductName><b>Product:</b> KD 15</ProductName>
+                                    <ProductId><b>ID:</b> 454677</ProductId>
+                                    <ProductColor color="#c5da90" />
+                                    <ProductSize><b>Size:</b> 42</ProductSize>
+                                </Details>
+                            </ProductDetail>
+                            <PriceDetail>
+                                <ProductAmountContainer>
+                                    <Remove />
+                                    <ProductAmount>2</ProductAmount>
+                                    <Add />
+                                </ProductAmountContainer>
+                                <ProductPrice>$ 80</ProductPrice>
+                            </PriceDetail>
+                        </Product>
+                        <Hr />
+                        <Product>
+                            <ProductDetail>
+                                <Image src="https://static.nike.com/a/images/t_default/7c578cde-85b3-4d9c-bc1d-989f03418f3e/lebron-19-basketball-shoes-2533G2.png" />
+                                <Details>
+                                    <ProductName><b>Product:</b> LEBRON'S 21</ProductName>
+                                    <ProductId><b>ID:</b> 455567</ProductId>
+                                    <ProductColor color="#6a54aa" />
+                                    <ProductSize><b>Size:</b> 41</ProductSize>
+                                </Details>
+                            </ProductDetail>
+                            <PriceDetail>
+                                <ProductAmountContainer>
+                                    <Remove />
+                                    <ProductAmount>2</ProductAmount>
+                                    <Add />
+                                </ProductAmountContainer>
+                                <ProductPrice>$ 90</ProductPrice>
+                            </PriceDetail>
+                        </Product>
+                    </Info>
+                    <Summary>
+                        <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+                        <SummaryItem>
+                            <SummaryItemText>Subtotal</SummaryItemText>
+                            <SummaryItemPrice>$ 170</SummaryItemPrice>
+                        </SummaryItem>
+                        <SummaryItem>
+                            <SummaryItemText>Estimated Shipping</SummaryItemText>
+                            <SummaryItemPrice>$ 5.90</SummaryItemPrice>
+                        </SummaryItem>
+                        <SummaryItem>
+                            <SummaryItemText>Shipping Discount</SummaryItemText>
+                            <SummaryItemPrice>$ -5.90</SummaryItemPrice>
+                        </SummaryItem>
+                        <SummaryItem type="total">
+                            <SummaryItemText>Total</SummaryItemText>
+                            <SummaryItemPrice>$ 170</SummaryItemPrice>
+                        </SummaryItem>
+                        <SummaryButton>CHECKOUT NOW</SummaryButton>
+                    </Summary>
+                </Bottom>
             </Wrapper>
             <Footer />
         </Container>
