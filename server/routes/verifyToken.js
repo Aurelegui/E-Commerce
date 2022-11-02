51cd962f4f19210e15fbe47ad31fbb7de436e5b7
AureleSecG <aurele.guitard.pro@gmail.com>
2022-11-02 23:35:26 +0100

    products add queries done

--- server/routes/verifyToken.js
+++ server/routes/verifyToken.js
@@ -24,4 +24,14 @@ const verifyTokenAuthorization = (req, res, next) => {
     });
 };
 
-module.exports = { verifyToken, verifyTokenAuthorization };
+const verifyTokenAndAdmin = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if (req.user.isAdmin) {
+            next();
+        } else {
+            res.status(403).json('You are not allowed to do that buddy Admin')
+        }
+    });
+};
+
+module.exports = { verifyToken, verifyTokenAuthorization, verifyTokenAndAdmin };
